75ffa4452834fd44785a76a87b38528fba1e6c03
dpopkov <pkvdenis@gmail.com>
2017-11-17 21:40:10 +0300

    Add java config.

--- src/main/java/learn/springinaction/knights/KnightMain.java
+++ src/main/java/learn/springinaction/knights/KnightMain.java
@@ -1,11 +1,11 @@
 package learn.springinaction.knights;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class KnightMain {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("knights-config.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(KnightConfig.class);
         Knight knight = context.getBean(Knight.class);
         knight.embarkOnQuest();
     }
