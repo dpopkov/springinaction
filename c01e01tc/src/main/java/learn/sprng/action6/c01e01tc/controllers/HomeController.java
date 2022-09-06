3ea100ec006a0113d79299396f1737bc5960b8e0
Denis Popkov <pkvdenis@gmail.com>
2022-09-06 16:15:39 +0300

    c01e01tc: implement home controller and view.

---
+++ c01e01tc/src/main/java/learn/sprng/action6/c01e01tc/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package learn.sprng.action6.c01e01tc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
